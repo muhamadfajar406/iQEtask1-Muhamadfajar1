590b685707f9552766ba7cb91f4655831a1b5d35
muhamad fajar <muhamadfajar015@gmail.com>
2021-11-11 22:26:50 +0700

    second commit

---
+++ part1/src/Persegi.java
@@ -0,0 +1,9 @@
+public class luasdankeliling {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
---
+++ part1/src/variable.java
@@ -0,0 +1,21 @@
+public class persegi {
+    int sisi = 4;
+
+    void Luas() {
+
+        int luas = sisi*sisi;
+        System.out.println("Luas persegi dengan sisi"+sisi+" adalah"+luas);
+
+    }
+
+    void  keliling(){
+
+        int keliling = 4 *sisi;
+        System.out.println("Keliling luas dengan sisi"+sisi+" adalah"+keliling);
+
+    }
+
+
+
+
+}
